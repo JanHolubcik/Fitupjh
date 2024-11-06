14e026337c74b6151a6fedeb2a2881a1fa307e81
Jan.Holubcik <jan.holubcik1@gmail.com>
2024-11-06 19:18:58 +0100

    feat: Changes made for livehosting

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
 import { authOptions } from "@/app/lib/auth";
 import NextAuth from "next-auth";
-const handler = NextAuth(authOptions) as never;
-export { handler as GET, handler as POST};
+export const { auth, signIn,  signOut, handlers: { GET, POST } }  = NextAuth(authOptions) as never;;
 
