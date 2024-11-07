5340d2baa8c13c7b45adbda96e964f3a412c0997
Jan.Holubcik <jan.holubcik1@gmail.com>
2024-11-07 19:28:51 +0100

    feat: Changes for livehosting

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider, useSession } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
 
+
 import { SpeedInsights } from '@vercel/speed-insights/next';
 export function Providers(
   { children }: { children: React.ReactNode }
