d15e6b33f18610b7280f5362d79605491a8da3cd
Jan.Holubcik <jan.holubcik1@gmail.com>
2024-11-10 18:42:45 +0100

    feat: Edited home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,37 @@ import { Image } from "@nextui-org/react";
 export default function Home() {
   return (
     <main className="dark  flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p>Black coffe yummi</p>
-        <div className="m-1 flex-none w-150 ...">
-          <Image
-            alt="nextui logo"
-            height={100}
+      <div className="max-w-[550px] flex flex-col justify-center items-center">
+      <Image
+      
+            className="flex self-center"
+            alt="Cute calc"
+            height={125}
             radius="sm"
-            src="https://www.themealdb.com/images/ingredients/Chicken.png"
-            width={100}
+            src="calc.png"
+            width={125}
           />
-        </div>
-        <h1 className="text-2xl font-bold mb-4">Food List</h1>
+        <h1 className="m-3 font-bold mt-8 text-3xl self-start">Welcome to FitUp - Calorie Tracker</h1>
+        <h2 className="m-3 font-bold self-start">Why FitUp?</h2>
+        <ul>
+          <li className="m-3">
+            <span className="font-bold">Simple and Intuitive:</span>{" "}
+            User-friendly interface makes logging your meals quick and
+            hassle-free.
+          </li>
+          <li className="m-3">
+            <span className="font-bold">Comprehensive Food Database:</span>{" "}
+            Access thousands of food items to record your intake accurately.
+          </li>
+          <li className="m-3">
+            <span className="font-bold">Nutritional Insights:</span> Beyond
+            calories, gain insight into the macronutrients in your diet for a
+            more comprehensive view of your health.
+          </li>
+        </ul>
+        <h2 className="m-3 font-bold self-start">Start your journey today!</h2>
+        <p className="m-3">Take the first step towards mindful eating and a healthier you. Register and transform the way you manage your nutrition.</p>
+
       </div>
     </main>
   );
