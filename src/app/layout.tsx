dd99cfa1e251fcb4dd091579807c90c9e3406887
Ján <jan.holubcik1@gmail.com>
2024-11-23 20:22:18 +0100

    feat: More change to session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <Providers session={session}>
+        <Providers>
           <NavbarComponent />
           {children}
         </Providers>
