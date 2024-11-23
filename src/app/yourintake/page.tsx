b2a5be0dceebebd94f5904509b9ec2ad3d8c4e6d
Ján <jan.holubcik1@gmail.com>
2024-11-23 19:44:57 +0100

    feat: Made session work

--- src/app/yourintake/page.tsx
+++ src/app/yourintake/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 import Food from "./Food";
+import { getServerSession } from "next-auth";
+import { SessionProvider } from "../SessionProvider";
 
-export default function YourIntake() {
-  return <Food></Food>;
+export default async function YourIntake() {
+  const session = await getServerSession();
+  return (
+    <SessionProvider session={session}>
+      <Food></Food>
+    </SessionProvider>
+  );
 }
