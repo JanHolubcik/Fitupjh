40f65a32b1cbf6f732f3ce601070ef5fe369932d
Ján <jan.holubcik1@gmail.com>
2024-11-23 22:09:11 +0100

    feat: Fix issue with session.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,19 @@ import { SessionProvider } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Session } from "next-auth";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({
+  session,
+  children,
+}: {
+  session: Session | null;
+  children: React.ReactNode;
+}) {
   return (
     <NextUIProvider>
       <SpeedInsights />
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider session={session}>{children}</SessionProvider>
     </NextUIProvider>
   );
 }
