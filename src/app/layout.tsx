bea43ef8e458229c59071c9da9d68178eb47841d
Ján <jan.holubcik1@gmail.com>
2026-05-19 18:10:40 +0200

    feat: Added import reflect-metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import 'reflect-metadata';
+
 
 import NavbarComponent from "@/components/Navbar/NavbarComponent";
 import { authOptions } from "@/lib/auth";
