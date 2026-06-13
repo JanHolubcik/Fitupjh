e8ccb0a732e9eb709e22c1995a5ed8718e2b4c91
Ján <jan.holubcik1@gmail.com>
2026-06-13 20:03:46 +0200

    feat: Prod fix.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["mongoose", "@typegoose/typegoose"],
-  },
+  serverExternalPackages: ["mongoose", "@typegoose/typegoose"],
 };
 
 export default nextConfig;
