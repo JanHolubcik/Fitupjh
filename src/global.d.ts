ce7e382b5972846704c1f0379830f5838a6ac2a5
Ján <jan.holubcik1@gmail.com>
2026-06-30 23:54:20 +0200

    feat: Fix for compiling svg and png.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,25 @@
 declare module "*.css";
+
+declare module "*.svg" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
+
+declare module "*.png" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+  };
+  export default content;
+}
